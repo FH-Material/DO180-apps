7585f1ffa20b232a09f9b4cf94c9491f7fe35b08
Student User <student@workstation.lab.example.com>
2023-03-18 11:56:47 -0400

    Added a change on march 18

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,6 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Another hello \n";
-print "A change is a comping by bm\n";
+print "A change is a coming by bm\n";
+print "Added another change on 230318\n";
 ?>
